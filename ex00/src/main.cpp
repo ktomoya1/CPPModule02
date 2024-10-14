d7d9a2a7a97c29528ad89f6c7fbe8d514207dfd4
Kudo Tomoya <ktomoya@c4r3s17.42tokyo.jp>
2024-10-14 10:53:06 +0900

    refact

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include "Fixed.h"
+
+#include <iostream>
 
 int main(void) {
   Fixed a;
@@ -10,5 +11,8 @@ int main(void) {
   std::cout << a.getRawBits() << std::endl;
   std::cout << b.getRawBits() << std::endl;
   std::cout << c.getRawBits() << std::endl;
+
+//  a.setRawBits(2);
+//  std::cout << a.getRawBits() << std::endl;
   return 0;
 }
